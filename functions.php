bedd2dc1c940ba84c3732ff53d8e88d36579156f
Sjoerd Koelewijn <s.koelewijn@gmail.com>
2020-10-05 20:28:26 +0200

    added favicon uri

--- functions.php
+++ functions.php
@@ -17,7 +17,7 @@ if(strpos($_SERVER['HTTP_HOST'], 'roxtar.local') !== false){
     include 'functions/acf.php';
 
 	function favicon() {
-		echo '<link rel="Shortcut Icon" type="image/x-icon" href="'. get_template_directory() . '/images/favicon.ico" />';
+		echo '<link rel="Shortcut Icon" type="image/x-icon" href="'. get_template_directory_uri() . '/images/favicon.ico" />';
 	}
 	add_action('wp_head', 'favicon');
 
